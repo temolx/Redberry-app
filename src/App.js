a14d4ca097ebc44d6c5c87fbc57346081db0b842
Temo <tlomsadze3@gmail.com>
2022-03-10 04:11:02 +0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useState } from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage';
 import PersonalInfo from './components/PersonalInfo';
 import Skills from './components/Skills';
@@ -32,21 +32,21 @@ function App() {
   });
 
   return (
-    <HashRouter>
+    <Router basename="/Reberry-app">
       <div className="App">
 
         <Routes>
-          <Route path="#/" element={<LandingPage />} />
-          <Route path="#/Personal" element={<PersonalInfo data={data} setData={setData} />} />
-          <Route path="#/Skills" element={<Skills data={data} setData={setData} />} />
-          <Route path="#/Covid" element={<Covid data={data} setData={setData} />} />
-          <Route path="#/Insights" element={<Insights data={data} setData={setData} />} />
-          <Route path="#/Submit" element={<Submit data={data} setData={setData} />} />
-          <Route path="#/Applications" element={<Applications />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/Personal" element={<PersonalInfo data={data} setData={setData} />} />
+          <Route path="/Skills" element={<Skills data={data} setData={setData} />} />
+          <Route path="/Covid" element={<Covid data={data} setData={setData} />} />
+          <Route path="/Insights" element={<Insights data={data} setData={setData} />} />
+          <Route path="/Submit" element={<Submit data={data} setData={setData} />} />
+          <Route path="/Applications" element={<Applications />} />
         </Routes>
         
       </div>
-    </HashRouter>
+    </Router>
   );
 }
 
